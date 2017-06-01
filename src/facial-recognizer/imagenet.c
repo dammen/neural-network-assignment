87364d2e9d4d112a725897f6f0e9d09bcad0818c
jonasdammen <jodammen93@gmail.com>
2017-06-01 13:25:34 +1000

    changed the skeleton code and implemented the facial recognizer. Implemented a new function output_result_on_imagelist, wich outputs if the NN classified a image correctly.

---
+++ src/facial-recognizer/imagenet.c
@@ -0,0 +1,83 @@
+/*
+ ******************************************************************
+ * HISTORY
+ * 15-Oct-94  Jeff Shufelt (js), Carnegie Mellon University
+ *      Prepared for 15-681, Fall 1994.
+ *
+ ******************************************************************
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "imagenet.h"
+
+#define TARGET_HIGH 0.9
+#define TARGET_LOW 0.1
+
+
+/*** This is the target output encoding for a network with one output unit.
+     It scans the image name, and if it's an image of me (js) then
+     it sets the target unit to HIGH; otherwise it sets it to LOW.
+     Remember, units are indexed starting at 1, so target unit 1
+     is the one to change....  ***/
+
+void load_target(IMAGE *img, BPNN *net)
+{
+  
+  char *names[20] = {"an2i", "at33", "boland", "bpm","ch4f","cheyer","choon","danieln","glickman","karyadi","kawamura","kk49","megak","mitchell","night","phoebe","steffi","sz24","saavik","tammo"};
+
+
+
+  int scale, i, j;
+  char userid[40], head[40], expression[40], eyes[40], photo[40];
+
+  userid[0] = head[0] = expression[0] = eyes[0] = photo[0] = '\0';
+
+  /*** scan in the image features ***/
+  sscanf(NAME(img), "%[^_]_%[^_]_%[^_]_%[^_]_%d.%[^_]",
+    userid, head, expression, eyes, &scale, photo);
+    int c = 0;
+    for(i = 0; i < 20; i++){
+        if (!strcmp(userid, names[i])){
+            c = i + 1;
+            net->target[c] = TARGET_HIGH;  /* it's me, set target to HIGH */
+            break;
+        }   
+    }
+    if(c != 0){ 
+        for(j = 1; j <= 20; j++){
+            if( j != c){
+                net->target[j] = TARGET_LOW; /* not me, set it to LOW */
+            }
+        }
+    }
+}
+
+
+/***********************************************************************/
+/********* You shouldn't need to change any of the code below.   *******/
+/***********************************************************************/
+
+void load_input_with_image(IMAGE *img, BPNN *net)
+{
+  double *units;
+  int nr, nc, imgsize, i, j, k;
+
+  nr = ROWS(img);
+  nc = COLS(img);
+  imgsize = nr * nc;;
+  if (imgsize != net->input_n) {
+    printf("LOAD_INPUT_WITH_IMAGE: This image has %d pixels,\n", imgsize);
+    printf("   but your net has %d input units.  I give up.\n", net->input_n);
+    exit (-1);
+  }
+
+  units = net->input_units;
+  k = 1;
+  for (i = 0; i < nr; i++) {
+    for (j = 0; j < nc; j++) {
+      units[k] = ((double) img_getpixel(img, i, j)) / 255.0;
+      k++;
+    }
+  }
+}
